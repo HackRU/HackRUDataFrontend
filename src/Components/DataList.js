25c9e739b2ee09bd012f725341b2cdb326d3e80a
Het Patel <hetpatel@Hets-MacBook-Air.local>
2024-09-25 16:33:24 -0400

    Added DataList to fetch data from backend

---
+++ src/Components/DataList.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const DataList = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get.get(`${process.env.REACT_APP_API_URL}/data`);
+                setData(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+          <h1>User Data</h1>
+          <ul>
+            {data.map((user, index) => (
+              <li key={index}>{JSON.stringify(user)}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    };
+    
+export default DataList;
