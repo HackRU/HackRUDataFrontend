66560bbb0fc572af88453e52d630f85fec669b4e
Het Patel <hetp0322@gmail.com>
2024-10-13 00:03:14 -0400

    Added POST request for custom query

--- src/api.js
+++ src/api.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
-const fetchData = async () => {
+const fetchData = async (query = null) => {
     try {
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/data`);
+        let response;
+        if (query) {
+            response = await axios.post(`${process.env.REACT_APP_API_URL}/data`, { query });
+        } else {
+            response = await axios.get(`${process.env.REACT_APP_API_URL}/data`);
+        }
         return response.data;
     } catch (error) {
         console.error('Error fetching data:', error);
         throw error;
     }
 };
+
 
 export default fetchData;
